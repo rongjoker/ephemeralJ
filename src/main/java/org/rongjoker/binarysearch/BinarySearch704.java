3a90dbc8c28bfb950ec502076dd3b6d0c3570c75
张士鹏 <zhangshipeng@shinho.com.cn>
2021-02-18 18:20:06 +0800

    迭代版本

--- src/main/java/org/rongjoker/binarysearch/BinarySearch704.java
+++ src/main/java/org/rongjoker/binarysearch/BinarySearch704.java
@@ -7,6 +7,7 @@ import org.junit.Test;
  * 704. 二分查找 https://leetcode-cn.com/problems/binary-search/
  * 典型的分治法，适用于有序数列
  * middle = start + (end — start) / 2
+ * 递归是最容易想出来的，实际上也可以用while循环
  *
  */
 public class BinarySearch704 {
@@ -14,11 +15,42 @@ public class BinarySearch704 {
     @Test
     public void test704() {
         int[] nums = {1,2,3,4,5};
-        System.out.println(search(nums, 2));
+        System.out.println(searchOptimize(nums, 2));
 
 
     }
 
+    /**
+     * 迭代版本，空间复杂度降低
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int searchOptimize(int[] nums, int target) {
+        int start =0,end=nums.length-1;
+
+        while (end>=start){
+            int middle = start + (end - start) / 2;
+
+            if(nums[middle]==target)
+                return middle;
+            else if(nums[middle]<target){
+                start = middle+1;
+            }else
+                end = middle-1;
+        }
+
+        return -1;
+
+    }
+
+
+    /**
+     * 递归版本
+     * @param nums
+     * @param target
+     * @return
+     */
     public int search(int[] nums, int target) {
         return binSearch(nums,target,0,nums.length-1);
     }
