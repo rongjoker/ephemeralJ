75d06a38c491aeb21ad1c73f2cbde9cd2726311a
joker <magic_of_java@foxmail.com>
2021-03-14 22:33:14 +0800

    85. 最大矩形(stock,84的迭代)

--- src/main/java/org/rongjoker/stock/MaximalRectangle85.java
+++ src/main/java/org/rongjoker/stock/MaximalRectangle85.java
@@ -2,11 +2,15 @@ package org.rongjoker.stock;
 
 import org.junit.Test;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * @date 03/14/2021
  * 85. 最大矩形 https://leetcode-cn.com/problems/maximal-rectangle/
  * 给定一个仅包含 0 和 1 、大小为 rows x cols 的二维二进制矩阵，找出只包含 1 的最大矩形，并返回其面积。
- * 类似221. 最大正方形
+ * 类似 84. 柱状图中最大的矩形
+ * 可以看作有rows x cols个圆柱，也就是把84重复cols次
  *
  *
  *
@@ -25,53 +29,58 @@ public class MaximalRectangle85 {
 
     public int maximalRectangle(char[][] matrix) {
 
-        int m = matrix.length, n = matrix[0].length, max = 0;
 
-        int [][][] dp = new int[m][n][2];
+        //控制边界条件
+        int m = matrix.length;
+        if(m==0)
+            return 0;
+        len = matrix[0].length;
+        if(len==0)
+            return 0;
+        extend_len=len+2;
+
+        cs = new int[extend_len];//添加哨兵
+        cs[0]=-1;
+        cs[len +1]=-1;
 
         for (int i = 0; i < m; i++) {
-            for (int j = 0; j <n; j++) {
-
-                if(matrix[i][j]=='1'){
-                    if(i>0){
-                        dp[i][j][0]=dp[i-1][j][0]+1;
-                    }else dp[i][j][0]=1;
-
-                    if(j>0){
-                        dp[i][j][1]=dp[i][j-1][0]+1;
-                    }else dp[i][j][1]=1;
-
-                    if(i>0 && j>0 && matrix[i-1][j-1]>1){
-
-                    }
-
-
-
-                    dp[i][j][0]=1;
-                    dp[i][j][1]=1;
-
-                    max = Math.max(dp[i][j][0] * dp[i][j][1],max);
-
-
-
-
-                }
-
-
-
-
-
-            }
-
-
-
+            largestRectangleArea(matrix,i);
         }
-
-
 
         return max;
 
     }
+
+
+    int[] cs;
+    int max=0, len,extend_len;
+    Deque<Integer> path = new ArrayDeque<>();
+
+
+    public void largestRectangleArea(char[][] matrix,int col) {
+
+        for (int i = 0; i < len; i++) {
+            if(matrix[col][i]=='0')
+                cs[i+1]=0;
+            else ++cs[i+1];
+        }
+
+        path.add(0);
+
+        for (int i = 1; i < extend_len; i++) {
+            while (cs[path.peekLast()]>cs[i]){
+                //i点为右侧,取出最高点后，path.getLast()为左侧
+                max=Math.max(max,cs[path.pollLast()]*(i-path.peekLast()-1));//(左右都比当前点要小，所以以当前点为高度的面积的宽度为右侧-左侧)
+            }
+            path.addLast(i);
+
+        }
+
+
+    }
+
+
+
 
 
 }
