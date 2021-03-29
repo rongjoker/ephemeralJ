f9b58225689cefb6f0b8412d2b19283b22f5fa92
张士鹏 <zhangshipeng@shinho.com.cn>
2021-03-29 19:45:43 +0800

    146. LRU 缓存机制

--- src/main/java/org/rongjoker/stack/LRUCache.java
+++ src/main/java/org/rongjoker/stack/LRUCache.java
@@ -12,6 +12,20 @@ import java.util.Map;
  */
 public class LRUCache {
 
+    public static void main(String[] args) {
+        LRUCache lRUCache = new LRUCache(2);
+        lRUCache.put(2, 1); // 缓存是 {1=1}
+        lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
+        System.out.println(lRUCache.get(2));    // 返回 1
+        lRUCache.put(1, 1); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+        lRUCache.put(4, 1); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+        System.out.println(lRUCache.get(2));;    // 返回 -1 (未找到)
+    }
+
+
+
+
+
     class DLinkedNode {
         int key;
         int value;
@@ -21,34 +35,103 @@ public class LRUCache {
         public DLinkedNode(int _key, int _value) {key = _key; value = _value;}
     }
 
-    private Map<Integer, DLinkedNode> cache = new HashMap<Integer, DLinkedNode>();
+    private Map<Integer, DLinkedNode> cache = new HashMap<>();
     private int size;
-    private int capacity;
+    private final int capacity;
     private DLinkedNode head, tail;
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
+        size = 0;
 
     }
 
     public int get(int key) {
         if(!cache.containsKey(key))return -1;
         DLinkedNode dLinkedNode = cache.get(key);
-
-
-
-        return key;
+        move2Head(dLinkedNode);
+        return dLinkedNode.value;
 
     }
 
     public void put(int key, int value) {
+        if(cache.containsKey(key)){
+            delete(cache.get(key));
+            --size;
+        }
+
+        DLinkedNode dLinkedNode = new DLinkedNode(key, value);
+        cache.put(key,dLinkedNode);
+        if(size==0){
+            tail = dLinkedNode;
+
+        }else if(size==capacity){
+            --size;
+            deleteTail();
+
+        }
+
+        move2Head(dLinkedNode);
+
+        ++size;
+
+
 
     }
 
-    public void deleteHead(){}
-    public void deleteTail(){}
+    private void delete(DLinkedNode element){
+        cache.remove(element.key);
+        if(tail==element) {
+            tail = element.prev;
+            return;
+        }
+        if(head==element) {
+            head = element.next;
+            return;
+        }
 
-    public void move2Head(){}
+        if(element.next!=null){
+            element.next.prev = element.prev;
+        }
+
+        if(element.prev!=null){
+            element.prev.next = element.next;
+        }
+
+    }
+
+    public void deleteTail(){
+        if(null!=tail){
+            cache.remove(tail.key);
+            tail = tail.prev;
+            tail.next=null;
+        }
+
+    }
+
+    public void move2Head(DLinkedNode dLinkedNode){
+
+        if(dLinkedNode==tail){
+            tail = dLinkedNode.prev;
+        }
+
+        if(dLinkedNode.next!=null){
+            dLinkedNode.next.prev = dLinkedNode.prev;
+        }
+
+        if(dLinkedNode.prev!=null){
+            dLinkedNode.prev.next = dLinkedNode.next;
+        }
+
+
+        if(head!=null){
+            dLinkedNode.next = head;
+            head.prev = dLinkedNode;
+        }
+
+        head = dLinkedNode;
+
+    }
 
 
 
