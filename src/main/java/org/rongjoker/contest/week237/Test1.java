a17b8c86776f535affc6ee663f16c5725f01a6cb
joker <magic_of_java@foxmail.com>
2021-04-18 21:51:48 +0800

    weekly-contest-237

--- src/main/java/org/rongjoker/contest/week237/Test1.java
+++ src/main/java/org/rongjoker/contest/week237/Test1.java
@@ -2,11 +2,34 @@ package org.rongjoker.contest.week237;
 
 import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Test1 {
 
     @Test
     public void test1(){
 
 //        System.out.println(minOperations(new int[]{1,1,1}));
+
+//        System.out.println(checkIfPangram("thequickbrownfoxjumpsoverthelazydog"));
+        System.out.println(checkIfPangram("leetcode"));
+    }
+
+    public boolean checkIfPangram(String sentence) {
+        int len = sentence .length();
+        if(len<26)return false;
+        Set<Character> set = new HashSet<>();
+        String sentence2 = "abcdefghijklmnopqrstuvwxyz";
+        for(int i = 0;i<26;++i){
+            set.add(sentence2.charAt(i));
+        }
+        for(int i=0;i<len;++i){
+            set.remove(sentence.charAt(i));
+            if(set.size()==0)return true;
+        }
+        return false;
+
+
     }
 }
--- src/main/java/org/rongjoker/contest/week237/Test2.java
+++ src/main/java/org/rongjoker/contest/week237/Test2.java
@@ -2,11 +2,51 @@ package org.rongjoker.contest.week237;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
+/**
+ * @date 04/18/2021
+ *
+ * 5735. 雪糕的最大数量 https://leetcode-cn.com/contest/weekly-contest-237/problems/maximum-ice-cream-bars/
+ */
 public class Test2 {
 
     @Test
     public void test2(){
 
-//        System.out.println(minOperations(new int[]{1,1,1}));
+        System.out.println(maxIceCream(new int[]{1,3,2,4,1},7));
     }
+
+    public int maxIceCream(int[] costs, int coins) {
+
+        int len = costs.length;
+
+        int[] dp = new int[coins+1];
+
+        for(int i=0;i<len;++i){
+            for(int j = coins;j>=costs[i];--j){
+                dp[j] = Math.max(dp[j], dp[j -costs[i]] +1);
+            }
+        }
+
+        return dp[coins];
+
+    }
+
+
+    //排名第一的优化版本
+    public int maxIceCream2(int[] costs, int coins) {
+
+        Arrays.sort(costs);
+        int ans = 0;
+        for(int c : costs){
+            if(c <= coins){
+                coins -= c;
+                ans++;
+            }
+        }
+        return ans;
+
+    }
+
 }
