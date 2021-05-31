b9c2473fba335eaf571beacc554942b1e773f4dc
张士鹏 <zhangshipeng@shinho.com.cn>
2021-05-31 19:00:16 +0800

    todo 402

---
+++ src/main/java/org/rongjoker/stack/RemoveKdigits402.java
@@ -0,0 +1,24 @@
+package org.rongjoker.stack;
+
+import org.junit.Test;
+
+/**
+ * @date 05/31/2021
+ * 402. 移掉K位数字 https://leetcode-cn.com/problems/remove-k-digits/
+ *
+ */
+public class RemoveKdigits402 {
+
+    @Test
+    public void test402(){}
+
+    public String removeKdigits(String num, int k) {
+        int len = num.length();
+        if(len<=k)return "0";
+
+
+        return null;
+
+
+    }
+}
