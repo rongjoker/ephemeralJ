e761dfbd6090bd8e74bc16245f0916281d12d8d3
joker <magic_of_java@foxmail.com>
2021-06-16 23:23:06 +0800

    todo

---
+++ src/main/java/org/rongjoker/binarysearch/SplitArray410.java
@@ -0,0 +1,28 @@
+package org.rongjoker.binarysearch;
+
+import org.junit.Test;
+
+/**
+ * @date 06/16/2021
+ * 410. 分割数组的最大值  https://leetcode-cn.com/problems/split-array-largest-sum/
+ *
+ * 给定一个非负整数数组 nums 和一个整数 m ，你需要将这个数组分成 m 个非空的连续子数组。
+ * 设计一个算法使得这 m 个子数组各自和的最大值最小。
+ *
+ */
+public class SplitArray410 {
+
+
+    @Test
+    public void test410(){}
+
+
+
+
+    public int splitArray(int[] nums, int m) {
+
+
+        return 0;
+
+    }
+}
--- src/main/java/org/rongjoker/ds/SplitString1849.java
+++ src/main/java/org/rongjoker/ds/SplitString1849.java
@@ -1,36 +1,66 @@
 package org.rongjoker.ds;
 
+import org.junit.Test;
+
 import java.util.ArrayDeque;
 import java.util.Deque;
 
 /**
  * @date 06/16/2021
- * 849. 将字符串拆分为递减的连续值 https://leetcode-cn.com/problems/splitting-a-string-into-descending-consecutive-values/
+ * 1849. 将字符串拆分为递减的连续值 https://leetcode-cn.com/problems/splitting-a-string-into-descending-consecutive-values/
+ *
+ * DFS解决，效率较低
  *
  *
  */
 public class SplitString1849 {
 
 
-    public boolean splitString(String s) {
 
-        int len = s.length();
-
-        if(len==cur) return true;
-        else return false;
-
-
+    @Test
+    public void test1849(){
+//        System.out.println(splitString("1234"));
+//        System.out.println(splitString("050043"));
+//        System.out.println(splitString("9080701"));
+        System.out.println(splitString("64424509442147483647"));
+        System.out.println(splitString("10690001068"));
     }
 
-    Deque<Integer> queue = new ArrayDeque<>();
+
+
+    public boolean splitString(String s) {
+        track(s, s.length(), 0);
+        return flag;
+    }
+
+    Deque<Long> queue = new ArrayDeque<>();
     int cur = 0;
+    boolean flag = false;
 
 
-    public void track(String s,int len,int start){
-        if(cur==len)return;
+    public void track(String s, int len, int start) {
+        if (cur == len) {
+            if(queue.size()>1)flag = true;
 
+            return;
+        }
 
+        for (int i = start; i < len; ++i) {
+            long in = 0;
+            try {
+                in = Long.parseLong(s.substring(start, i + 1));
+            } catch (NumberFormatException e) {
+                break;
+            }
+            if (queue.isEmpty() || queue.peekLast() == in + 1) {
+                queue.addLast(in);
+                cur += (i - start + 1);
+                track(s, len, i + 1);
+                cur -= (i - start + 1);
+                queue.pollLast();
+            }
 
+        }
 
     }
 }
