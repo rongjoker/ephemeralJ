c6046802908e8a2787e81e65b3bf0393105e846f
joker <magic_of_java@foxmail.com>
2021-06-18 00:03:43 +0800

    边界问题

--- src/main/java/org/rongjoker/binarysearch/SearchInRotatedSortedArray33.java
+++ src/main/java/org/rongjoker/binarysearch/SearchInRotatedSortedArray33.java
@@ -16,8 +16,34 @@ public class SearchInRotatedSortedArray33 {
     @Test
     public void test33(){
 
-        int[] nums = {4,5,6,7,0,1,2};
-        System.out.println(search(nums,0));
+//        System.out.println(search2(new int[]{4, 5, 6, 7, 0, 1, 2},0));
+//        System.out.println(search2(new int[]{4,5,6,7,0,1,2},3));
+//        System.out.println(search2(new int[]{1},0));
+        System.out.println(search2(new int[]{1,3},3));
+
+    }
+
+
+    public int search2(int[] nums, int target) {
+        int left = 0,right = nums.length -1;
+        while(left<=right){
+            int mid = left + ((right - left)>>1);
+            if(nums[mid]==target)return mid;
+
+            if(nums[mid]<nums[right]){
+                if(nums[mid]<target && nums[right]>target)left = mid +1;
+                else right = mid-1;
+            }else{
+                if(nums[left]<target && nums[mid]>target)right = mid-1;
+                else left = mid+1;
+
+            }
+        }
+
+
+        if(nums[left]==target)return left;
+        return -1;
+
 
     }
 
