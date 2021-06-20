8d284000195215e4e3d28a76636fed878d9e761e
张士鹏 <zhangshipeng@shinho.com.cn>
2021-06-20 10:20:19 +0800

    week-contest-246

---
+++ src/main/java/org/rongjoker/contest/week246/Test2.java
@@ -0,0 +1,7 @@
+package org.rongjoker.contest.week246;
+
+public class Test2 {
+
+
+
+}
---
+++ src/main/java/org/rongjoker/contest/week246/Test3.java
@@ -0,0 +1,14 @@
+package org.rongjoker.contest.week246;
+
+import org.junit.Test;
+
+public class Test3 {
+
+    @Test
+    public void test3() {
+
+    }
+
+
+
+}
