79696dae31d5b02f4971d9969c2f0a172d100685
zhangshipeng <magic_of_java@foxmail.com>
2021-08-18 16:24:02 +0800

    StringFormat

--- src/main/java/org/rongjoker/feature/StringFormatTemplate.java
+++ src/main/java/org/rongjoker/feature/StringFormatTemplate.java
@@ -14,6 +14,12 @@ public class StringFormatTemplate {
     @Test
     public void testStringFormat(){
 
+        int[] nums = new int[]{1,2,3,4,5,6};
+
+        String code2 = String.format("codes:%s",nums);
+
+        System.out.printf("code2:%s",code2);
+
         String code = String.format("my name is %s,i am %s so far%n","joker",31);
 
         System.out.printf("i am writing codes like this:%s",code);
