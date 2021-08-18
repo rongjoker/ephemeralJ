efc0f7d0ce6a2482f64c331307c7b85bc0470051
zhangshipeng <magic_of_java@foxmail.com>
2021-08-18 15:57:15 +0800

    26与80

--- src/main/java/org/rongjoker/sw/RemoveDuplicatesFromSortedArray26.java
+++ src/main/java/org/rongjoker/sw/RemoveDuplicatesFromSortedArray26.java
@@ -19,7 +19,25 @@ public class RemoveDuplicatesFromSortedArray26 {
     public void test26(){
 
         System.out.println(removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,4}));
+        System.out.println(removeDuplicates2(new int[]{0,0,1,1,1,2,2,3,3,4}));
 
+
+    }
+
+    public int removeDuplicates2(int[] nums) {
+        int len = nums.length;
+        if(len<2)return len;
+        int left=0;
+        int right=1;
+        while (right<len){
+            if (nums[left]!=nums[right]){
+                left++;
+                if(right>left)nums[left] = nums[right];
+            }
+
+            right++;
+        }
+        return left+1;
 
     }
 
