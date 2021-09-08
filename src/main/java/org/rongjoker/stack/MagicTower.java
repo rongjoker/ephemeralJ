14b980a38d0fe2fa7a65b8ba3ab007e0d9762cba
zhangshipeng <magic_of_java@foxmail.com>
2021-09-08 21:36:56 +0800

    堆+贪心

--- src/main/java/org/rongjoker/stack/MagicTower.java
+++ src/main/java/org/rongjoker/stack/MagicTower.java
@@ -1,13 +1,11 @@
-package org.rongjoker.array;
+package org.rongjoker.stack;
 
-import java.util.Deque;
-import java.util.LinkedList;
+import java.util.PriorityQueue;
 import org.junit.Test;
 
 /**
- *
- *  LCP 30. 魔塔游戏
- *小扣当前位于魔塔游戏第一层，共有 N 个房间，编号为 0 ~ N-1。每个房间的补血道具/怪物对于血量影响记于数组 nums，其中正数表示道具补血数值，即血量增加对应数值；负数表示怪物造成伤害值，即血量减少对应数值；0 表示房间对血量无影响。
+ * LCP 30. 魔塔游戏
+ * 小扣当前位于魔塔游戏第一层，共有 N 个房间，编号为 0 ~ N-1。每个房间的补血道具/怪物对于血量影响记于数组 nums，其中正数表示道具补血数值，即血量增加对应数值；负数表示怪物造成伤害值，即血量减少对应数值；0 表示房间对血量无影响。
  *
  * 小扣初始血量为 1，且无上限。假定小扣原计划按房间编号升序访问所有房间补血/打怪，为保证血量始终为正值，小扣需对房间访问顺序进行调整，每次仅能将一个怪物房间（负数的房间）调整至访问顺序末尾。请返回小扣最少需要调整几次，才能顺利访问所有房间。若调整顺序也无法访问完全部房间，请返回 -1。
  *
@@ -30,47 +28,43 @@ import org.junit.Test;
  * 输出：-1
  *
  * 解释：调整访问顺序也无法完成全部房间的访问。
- *
- *
  */
 public class MagicTower {
 
     @Test
-    public void testMagicTower(){
-        System.out.println(magicTower(new int[]{100,100,100,-250,-60,-140,-50,-50,100,150}));
-        System.out.println(magicTower(new int[]{-200,-300,400,0}));
+    public void testMagicTower() {
+        System.out.println(magicTower(new int[]{100, 100, 100, -250, -60, -140, -50, -50, 100, 150}));
+        System.out.println(magicTower(new int[]{-200, -300, 400, 0}));
 
     }
 
 
     public int magicTower(int[] nums) {
 
-        Deque<Integer> queue = new LinkedList<>();
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
 
         long cur = 1;
         for (int num : nums) {
-            queue.addLast(num);
-            cur+= num;
+            cur += num;
         }
-        if(cur<=0)return -1;
+        if (cur <= 0) {
+            return -1;
+        }
 
-        cur  =1;
+        cur = 1;
         int ans = 0;
 
-        while (!queue.isEmpty()){
-            int temp = queue.peekFirst();
-            if(temp>0) cur +=queue.pollFirst();
-            else if (temp==0) queue.pollFirst();
-            else{
-                if(cur + temp >0)cur +=queue.pollFirst();
-                else {
-                    queue.addLast(queue.pollFirst());
-                    ans++;
+        for (int num : nums) {
+            cur += num;
 
-                }
+            if (num < 0) {
+                queue.offer(num);
+            }
+            if (cur <= 0) {
+                cur -= queue.poll();
+                ans++;
             }
         }
-
         return ans;
 
     }
